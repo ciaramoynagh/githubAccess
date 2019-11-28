6b97983762303a6d200e82d316de12fc4c472cb9
ciaramoynagh <moynaghc@tcd.ie>
2019-11-28 18:03:06 +0000

    final updates

--- src/index.js
+++ src/index.js
@@ -6,3 +6,5 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
+
+/* this code is adapted from a Hackernoon tutorial https://medium.com/hackernoon/building-github-profile-analytics-using-react-part-1-37e03b0c3366 */
